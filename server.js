f16d5fbb736eb421e3d730a1725b9d7c2847f9f0
PrasetyoAdji28 <95466718+JrBeller28@users.noreply.github.com>
2026-03-30 13:20:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,15 +6,20 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-// GANTI DENGAN URL MONGODB KAMU
-mongoose.connect('mongodb+srv://admin:parahita2026@cluster0.hrloeif.mongodb.net/parahita?retryWrites=true&w=majority')
-.then(()=>console.log('MongoDB Connected'))
-.catch(err=>console.log('Mongo Error:', err))
+// DEBUG GLOBAL
+process.on('uncaughtException', err => {
+  console.error('UNCAUGHT ERROR:', err)
+})
 
-// ROOT TEST
+// ROOT
 app.get('/', (req,res)=>{
   res.send('Backend jalan 🚀')
 })
+
+// CONNECT MONGODB
+mongoose.connect('mongodb+srv://admin:parahita2026@cluster0.hrloeif.mongodb.net/parahita?retryWrites=true&w=majority')
+.then(()=>console.log('MongoDB Connected'))
+.catch(err=>console.log('Mongo Error:', err))
 
 // SCHEMA
 const OrderSchema = new mongoose.Schema({
@@ -28,18 +33,23 @@ const Order = mongoose.model('Order', OrderSchema)
 
 // ROUTES
 app.post('/orders', async (req,res)=>{
-  const order = new Order(req.body)
-  await order.save()
-  res.json({success:true})
+  try {
+    const order = new Order(req.body)
+    await order.save()
+    res.json({success:true})
+  } catch(err){
+    console.log(err)
+    res.status(500).json({error:'Server error'})
+  }
 })
 
 app.get('/orders', async (req,res)=>{
-  const orders = await Order.find()
-  res.json(orders)
+  const data = await Order.find()
+  res.json(data)
 })
 
-// PORT
+// PORT WAJIB
 const PORT = process.env.PORT || 3000
-app.listen(PORT, () => {
-  console.log('Server jalan di port ' + PORT)
+app.listen(PORT, ()=>{
+  console.log('Running on port ' + PORT)
 })
