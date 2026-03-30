20864444cd6559da21fa2bf8a561b78a77cd58c2
PrasetyoAdji28 <95466718+JrBeller28@users.noreply.github.com>
2026-03-30 12:12:39 +0700

    Update server.js

--- server.js
+++ server.js
@@ -6,17 +6,7 @@ const app = express()
 app.use(cors())
 app.use(express.json())
 
-app.post('/login',(req,res)=>{
-  const {username,password} = req.body
-
-  if(username==='admin' && password==='admin123'){
-    res.json({success:true})
-  } else {
-    res.json({success:false})
-  }
-})
-
-// CONNECT MONGODB
+// GANTI DENGAN URL MONGODB KAMU
 mongoose.connect('mongodb+srv://admin:<db_password>@cluster0.hrloeif.mongodb.net/?appName=Cluster0')
 .then(()=>console.log('MongoDB Connected'))
 
@@ -30,17 +20,32 @@ const OrderSchema = new mongoose.Schema({
 
 const Order = mongoose.model('Order', OrderSchema)
 
-// CREATE ORDER
+
+// ================= LOGIN =================
+app.post('/login',(req,res)=>{
+  const {username,password} = req.body
+
+  if(username==='admin' && password==='admin123'){
+    res.json({success:true})
+  } else {
+    res.json({success:false})
+  }
+})
+
+
+// ================= CREATE ORDER =================
 app.post('/orders', async (req,res)=>{
   const order = new Order(req.body)
   await order.save()
   res.json({success:true})
 })
 
-// GET ORDERS
+
+// ================= GET ORDERS =================
 app.get('/orders', async (req,res)=>{
   const orders = await Order.find().sort({created_at:-1})
   res.json(orders)
 })
+
 
 app.listen(3000,()=>console.log('Server jalan di port 3000'))
